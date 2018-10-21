8baf13fe3e3f92af5598fe5f473493cfd1dabbcf
leolee18 <wwwliyuxi@163.com>
2018-10-21 18:25:58 +0800

    增加样式和内容

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+$('#logId').velocity('callout.pulse', { duration: 650});
+$('#dianId').velocity('transition.slideRightIn', { duration: 650,delay: 200 });
+
+$('#tit1Id').velocity('transition.slideDownBigIn', { duration: 1000,delay: 300 });
+$('#tit2Id').velocity('transition.slideLeftBigIn', { duration: 1500,delay: 300 });
+$('#tit3Id').velocity('transition.slideUpBigIn', { duration: 1000,delay: 500 });
+$('#tit4Id').velocity('transition.slideRightBigIn', { duration: 700,delay: 1000 });
+
+$('#anim1').velocity({opacity: 1,top:'-=50px'}, { duration: 600,delay: 100});
+$('#anim2').velocity({opacity: 1,top:'-=50px'}, { duration: 600,delay: 200});
+$('#anim3').velocity({opacity: 1,top:'-=50px'}, { duration: 600,delay: 300});
+$('#anim4').velocity({opacity: 1,top:'-=50px'}, { duration: 600,delay: 400});
+$('#anim5').velocity({opacity: 1,top:'-=50px'}, { duration: 600,delay: 500});
+$('#anim6').velocity({opacity: 1,top:'-=50px'}, { duration: 600,delay: 600});
+$('#anim7').velocity({opacity: 1,top:'-=50px'}, { duration: 600,delay: 700});
+$('#anim8').velocity({opacity: 1,top:'-=50px'}, { duration: 600,delay: 800});
